f378b21c31fb655be511ba5bd5e84f719b292132
Amy Welsh <awelsh@davidsonacademy.com>
2021-06-17 16:09:52 -0500

    castle & robots playtime

---
+++ castle_game.py
@@ -0,0 +1,24 @@
+# Castle Game pg. 21
+
+print ("You are in a dark room in a mysterious castle.")
+
+print ("In front of you are three doors.  You must choose one.")
+player_choice=input("Choose 1, 2 or 3...")
+
+if player_choice=="1":
+    print("You find a room full of treasure.  You're rich!")
+    print("GAME OVER - YOU WIN!")
+
+elif player_choice=="2":
+    print("The door opens and an angry ogre hits you with his club.")
+    print("GAVE OVER.  YOU LOSE.")
+
+elif player_choice=="3":
+    print("You go into the room and find a sleeping dragon.")
+    print("The dragon wakes up and eats you.  You are delicious.")
+    print("GAME OVER.  YOU LOSE.")
+
+else:
+    print("Try following directions.")
+
+    
---
+++ robots.py
@@ -0,0 +1,16 @@
+# Do you like robots?
+user_reply=input("Do you like robots? (Type Yes or No)")
+# If yes, then hello!
+if user_reply=="Yes":
+    print("Beep Boop!")
+
+# If no, they don't like you either
+elif user_reply=="No":
+    print("Well, robots don't like you either.")
+
+# Easter egg - laser eyes - watch out
+elif user_reply=="only ones with laser eyes":
+    print("ZZAPP")
+
+else:
+    print("Your human nonsense offends us.")
